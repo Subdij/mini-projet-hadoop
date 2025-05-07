6bde2623642b871aaf28d05c61d9fb8e82e20b9e
ziyadou333@gmail.com <ziyadou2011@hotmail.fr>
2025-05-07 09:43:41 +0200

    fix mapper python3

--- python3/mapper.py
+++ python3/mapper.py
@@ -3,26 +3,17 @@ import csv
 import os
 
 def mapping():
-    input_file = 'nat2022_valid.csv'
-    
-    # Vérifier si le fichier d'entrée existe
-    if not os.path.exists(input_file):
-        print(f"Erreur: Le fichier {input_file} n'existe pas.")
-        return
-    
     try:
-        with open(input_file, 'r', encoding='utf-8') as infile:
-             
-            # Créer le lecteur et l'écrivain CSV
-            reader = csv.reader(infile, delimiter=';')
+        # Utiliser sys.stdin au lieu d'un fichier spécifique
+        reader = csv.reader(sys.stdin, delimiter=';')
             
-            # Ignorer l'en-tête
-            header = next(reader, None)
+        # Ignorer l'en-tête
+        header = next(reader, None)
             
-            for row in reader:
-                sexe, preusuel, annais, dpt, nombre = row
-                key = f"{annais}_{sexe}_{preusuel}"
-                print(f"{key}\t{nombre}")
+        for row in reader:
+            sexe, preusuel, annais, dpt, nombre = row
+            key = f"{annais}_{sexe}_{preusuel}"
+            print(f"{key}\t{nombre}")
             
             
     except Exception as e:
