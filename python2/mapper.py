eb497af9d466f4f5351f21141226f3be5ae16615
ziyadou333@gmail.com <ziyadou2011@hotmail.fr>
2025-05-07 09:38:04 +0200

    fix mapper

--- python2/mapper.py
+++ python2/mapper.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python2
 # filepath: c:\Users\ziyad\OneDrive\Bureau\mini-projet-hadoop\python2\mapper.py
 import csv
-import os
 import sys
 
 def mapping():
-    input_file = 'nat2022_valid.csv'
-    
-    # Verifier si le fichier d'entree existe
-    if not os.path.exists(input_file):
-        print "Erreur: Le fichier %s n'existe pas." % input_file
-        return
-    
     try:
-        with open(input_file, 'r') as infile:
-             
-            # Creer le lecteur CSV
-            reader = csv.reader(infile, delimiter=';')
+        # Utiliser sys.stdin au lieu d'un fichier spécifique
+        reader = csv.reader(sys.stdin, delimiter=';')
             
-            # Ignorer l'en-tete
-            header = next(reader, None)
+        # Ignorer l'en-tête
+        header = next(reader, None)
             
-            for row in reader:
-                sexe, preusuel, annais, dpt, nombre = row
-                key = "{0}_{1}_{2}".format(annais, sexe, preusuel)
-                print "{0}\t{1}".format(key, nombre)
+        for row in reader:
+            sexe, preusuel, annais, dpt, nombre = row
+            key = "{0}_{1}_{2}".format(annais, sexe, preusuel)
+            print "{0}\t{1}".format(key, nombre)
             
     except Exception as e:
         print "Une erreur s'est produite: %s" % e
